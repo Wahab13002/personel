8963aa1aced87ba70c9d6b3b90cb7196f5b5b0ab
ALLAOUI El wahaby <el-wahaby.allaoui@etu.univ-amu.fr>
2021-06-29 21:44:21 +0200

    fix carousel 70%: missing arrows + animation

--- src/app/mosquee/mosquee.component.ts
+++ src/app/mosquee/mosquee.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mosquee.component.css']
 })
 export class MosqueeComponent implements OnInit {
-
+  images = [];
   constructor() { }
 
   ngOnInit() {
+    this.images = [
+      "assets/img/coran.png",
+      "assets/img/fotolia.jpg",
+      "assets/img/logo1.png",
+      "assets/img/logo2.jpg",
+    ];
   }
 
 }
