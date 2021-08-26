191ba5c6722f8b121544e2266e2f74a18ab44589
ALLAOUI El wahaby <el-wahaby.allaoui@etu.univ-amu.fr>
2021-08-26 19:47:55 +0200

    images + mail smpt 60%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,7 +37,9 @@ import { InformationPrayerTimeComponent } from './information-prayer-time/inform
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -8,8 +16,37 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() { }
-
+  myform: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    message: new FormControl(''),
+  });;
+  email: FormControl ;
+  message: FormControl;
   ngOnInit() {
+  }
+
+  onsubmit() {
+
+
+
+    
+
+    console.log('...')
+      console.log('0')
+    fetch("//formspree.io/f/mleaoywb", {
+      method: 'POST',
+      body: this.myform.toString(),
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+        console.log('1')
+      this.myform.reset()
+    }).catch(error => {
+      console.log('2')
+    });
+        
+
   }
 
 }
